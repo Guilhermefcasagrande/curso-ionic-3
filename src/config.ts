22cb861f3912d495df1d10dc043212c08219f65a
Guilherme Francisco Casagrande <guilhermefranciscocasagrande@gmail.com>
2019-01-17 21:04:29 -0200

    Puxando as postagens da api

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 // https://sujeitoprogramador.com/nutriapp/wp-json/wp/v2/posts
-
 export const WORDPRESS_URL = 'https://sujeitoprogramador.com/nutriapp/';
 export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
 
--- src/pages/dicas/dicas.ts
+++ src/pages/dicas/dicas.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { HomePage } from '../home/home';
+import { WordpressService } from './../../services/wordpress.service';
 
 
 @IonicPage()
@@ -11,11 +12,39 @@ import { HomePage } from '../home/home';
 })
 export class DicasPage {
 
+	posts: Array<any> = new Array <any>();
+	morePagesAvailable: boolean = true;
+
 	constructor(public navCtrl: NavController, 
 				public navParams: NavParams, 
 				public fire: AngularFireAuth, 
-				public toatsCtrl: ToastController) {
+				public toatsCtrl: ToastController,
+				public loadingCtrl: LoadingController,
+				public wordpressService: WordpressService) {
 					
+	}
+
+	// Função do próprio ionic
+	ionViewWillEnter(){
+		this.morePagesAvailable = true;
+
+		if(!(this.posts.length > 0)){
+			let loading = this.loadingCtrl.create();
+			loading.present();
+
+			this.wordpressService.getRecentPosts()
+			.subscribe(data => {
+				console.log('dados das postagens: ', data);
+				for(let post of data){
+					// Remove as tags html que vem no conteúdo
+					post.excerpt.rendered = post.excerpt.rendered.split('<a')[0] + '<p>';
+
+					this.posts.push(post);
+				}
+
+				loading.dismiss();
+			});
+		}
 	}
 
 	logout(){
