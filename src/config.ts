21ef58de7452e13d4fff0084affa95c6054a5d63
Guilherme Francisco Casagrande <guilhermefranciscocasagrande@gmail.com>
2019-01-17 20:37:13 -0200

    Criado service para trazer os post pela api

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// https://sujeitoprogramador.com/nutriapp/wp-json/wp/v2/posts
+
+export const WORDPRESS_URL = 'https://sujeitoprogramador.com/nutriapp/';
+export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
+
